24d51f1b6642920d79759f8ae5fee63f67e7b8f7
David Drysdale <david.drysdale@gmail.com>
2021-03-13 19:58:02 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,5 @@
 module.exports = function(eleventyConfig) {
 
-  const format = require('date-fns/format')
-
-  module.exports = function (config) {
-
-// add `date` filter
-  config.addFilter('date', function (date, dateFormat) {
-    return format(date, dateFormat)
-  })
-
-    const markdownIt = require('markdown-it');
-    const markdownItOptions = {
-        html: true,
-        linkify: true
-    };
 
     const md = markdownIt(markdownItOptions)
     .use(require('markdown-it-footnote'))
